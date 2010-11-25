801599d8b3421243064ef5658a0eff74bcccfb3f
xander <xander@5b663021-50af-43ff-b51e-cd5118c65b19>
2010-11-25 06:52:48 +0000

    First sync with svn

---
+++ shareit/shared/sql_config.php
@@ -0,0 +1,7 @@
+<?php // shared/sql_config.php
+$db_hostname = 'db.shareit.skyrien.com';
+$db_database = 'shareitdb';
+$db_username = 'jooreef';
+$db_password = 'bitchass1';
+
+?>
---
+++ shareit/shared/test.php
@@ -0,0 +1,5 @@
+<?php
+include_once 'sql_comm.php';
+echo "This is a test message<br>";
+
+?>
