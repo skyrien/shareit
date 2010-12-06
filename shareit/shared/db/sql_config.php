19d1581cf3569dc958064c42fb7ffb73f34a8265
jooreef <jooreef@5b663021-50af-43ff-b51e-cd5118c65b19>
2010-12-06 07:35:55 +0000

    more updates to facebook auth code

---
+++ shareit/shared/db/sql_config.php
@@ -0,0 +1,12 @@
+<?php // shared/sql_config.php
+define(DB_HOSTNAME,'db.shareit.skyrien.com');
+define(DB_SHAREIT_DATABASE, 'shareitdb');
+define(DB_USERNAME, 'jooreef');
+define(DB_PASSWORD, 'bitchass1');
+//Keep for now till xander updates his code
+$db_hostname = 'db.shareit.skyrien.com';
+$db_database = 'shareitdb';
+$db_username = 'jooreef';
+$db_password = 'bitchass1';
+
+?>
